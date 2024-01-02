5250dbfc94fcaf36cb061ad9ec495f954f2ecd82
athomas227 <andrewthomas@pursuit.org>
2024-01-02 18:59:00 -0500

    finished exercise

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const click = <button onClick={() => alert("Button Pressed")}>Click Me</button>;
-const parts = ( 
-<React.Fragment>
-  <h1>Welcome to React</h1>
-  <p>Hello, I am Andrew and I want to be a software engineer</p>
-</React.Fragment>);
-
 function App() {
+  const click = <button onClick={() => alert("Button Pressed")}>Click Me</button>;
+  
+  const parts = ( 
+  <React.Fragment>
+    <h1>Welcome to React</h1>
+    <p>Hello, I am Andrew and I want to be a software engineer</p>
+  </React.Fragment>);
+
   return (
     <div className="App">
       <header className="App-header">
